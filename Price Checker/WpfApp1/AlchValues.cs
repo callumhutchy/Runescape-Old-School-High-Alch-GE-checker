13fe6e42fdeb1ebb37216a6afa29a192af1c6e8d
callumhutchy <callumhutchie@hotmail.co.uk>
2018-05-12 00:09:14 +0100

    Main Search program slightly works, think the server might be rejecting a lot of the requests

---
+++ Price Checker/WpfApp1/AlchValues.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class AlchValues
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int alch_value { get; set; }
+
+    }
+}
--- Price Checker/WpfApp1/MainWindow.xaml.cs
+++ Price Checker/WpfApp1/MainWindow.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Shapes;
 using System.Net;
 using System.IO;
 using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace WpfApp1
 {
@@ -23,28 +26,139 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        static string highalchprices = "newoutput.json";
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        
+        static int natureRunePrice = 0;
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
+            string json = File.ReadAllText(highalchprices);
 
-            var w = new WebClient();
-            var json_data = string.Empty;
-            string url = "http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=" + txtId.Text;
-            txtOutput.Text += url;
-                json_data = w.DownloadString(url);
-         
+            List<AlchValues> alchv = new List<AlchValues>();
+            
+            json = json.Replace("[", "");
+            json = json.Replace("]", "");
 
-            txtOutput.Text += json_data;
+            string[] temp = json.Split(',');
 
+            List<string> product = new List<string>();
 
+            List<string> tempList = new List<string>(temp);
+            while(tempList.Count > 0)
+            {
+                product.Add(tempList[0] +","+ tempList[1] + ","+ tempList[2]);
+                for (int i = 0; i < 3; i++)
+                {
+                    tempList.RemoveAt(0);
+                }
+            }
+
+            foreach(string item in product)
+            {
+                alchv.Add(JsonConvert.DeserializeObject<AlchValues>(item));
+            }
+
+            //txtOutput.Text += alchv.Count;
 
             
+            natureRunePrice = GetPriceOfNatureRune();
+            Console.WriteLine(natureRunePrice);
+            //txtOutput.Text += natureRunePrice;
+
+
+
+            foreach(AlchValues alch in alchv)
+            {
+                int currentPrice = GetPriceOfItem(alch.id);
+                Console.WriteLine(alch.id + ": " + alch.name + " " + currentPrice + " " + alch.alch_value);
+                if(currentPrice < 0)
+                {
+                    continue;
+                }
+                if((currentPrice + natureRunePrice + 200) < alch.alch_value)
+                {
+                    txtOutput.Text += alch.name + " " + currentPrice + " " + alch.alch_value;
+                }
+                
+            }
+
+
+                        
         }
+
+        int GetPriceOfItem(int id)
+        {
+            var w = new WebClient();
+            var json_data = string.Empty;
+            string url = "http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=" + id.ToString();
+            json_data = w.DownloadString(url);
+            Console.WriteLine(json_data);
+            if (json_data == "")
+            {
+                
+                return -2;
+            }
+
+            return ParseNumber(CreateItemFromJson(json_data).current.price);
+        }
+
+        int ParseNumber(string num)
+        {
+            double result;
+            if (double.TryParse(num, out result))
+            {
+
+                return (int) result;
+
+            }
+            else
+            {
+                if (num.ToLower().Contains("k"))
+                {
+                    num = num.Replace("k", "");
+                    if(double.TryParse(num, out result))
+                    {
+                        result = result * 1000;
+                        return (int) result;
+                    }
+
+                }
+            }
+            return -3;
+        }
+
+        int GetPriceOfNatureRune()
+        {
+            var w = new WebClient();
+            var json_data = string.Empty;
+            string url = "http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item=561";
+            json_data = w.DownloadString(url);
+            
+            return ParseNumber(CreateItemFromJson(json_data).current.price);
+                       
+        }
+
+        Item CreateItemFromJson(string json)
+        {
+            Item item = new Item();
+
+            JsonTextReader r = new JsonTextReader(new StringReader(json));
+
+            if(json == "")
+            {
+                return null;
+            }
+
+            item = JsonConvert.DeserializeObject<RootObject>(json).item;
+            
+            return item;
+        }
+
     }
 }
